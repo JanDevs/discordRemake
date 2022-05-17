67375770e3292db9ae6235d1bc1b55083cec22df
Jan Juarez <janlolsas272272@gmail.com>
2022-05-16 20:58:24 -0500

    Agregada la fuente y se cambio el logo por svg

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,34 @@
 <template>
   <div id="nav">
+    <Nav> </Nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/index">About</router-link>
   </div>
   <router-view />
 </template>
+<script>
+import Nav from '@/components/Nav.vue';
+export default {
+  name: "App",
+  components: {
+    Nav,
+  }
+}
+</script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Uni-Sans", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  padding: none;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+@font-face {
+  font-family: "Uni-Sans";
+  src: local("Uni-Sans"), url(./assets/fonts/UniSansSBold.otf) format("truetype");
 }
 
 #nav a.router-link-exact-active {
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,15 +1,13 @@
 <template>
-  <div class="hello">
-    
-  </div>
+    holis
 </template>
-
 <script>
 export default {
-  name: "HelloWorld",
+  name: "Index",
   props: {
-    msg: String,
+    
   },
+
 };
 </script>
 
